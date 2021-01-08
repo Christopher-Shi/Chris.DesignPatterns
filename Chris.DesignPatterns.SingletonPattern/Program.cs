368ae3c00ab8dc1898594542cb02ff4256274de9
shixiaoyong <814740187@qq.com>
2021-01-08 23:39:17 +0800

    added 单例模式

---
+++ Chris.DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chris.DesignPatterns.SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
